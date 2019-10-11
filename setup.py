29acda3edb44a46648a62875a35dbd590e4c9b2f
Sanjeev <nandal4246@outlook.com>
2019-10-11 00:01:05 -0400

    first commit

---
+++ setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='websocket_server',
+    version='0.4',
+    packages=find_packages("."),
+    url='https://github.com/nandal4246/testwebsocket.git',
+    license='MIT',
+    author='Sanjeev Nandal',
+    author_email='nandal1504246@gmail.com',
+    install_requires=[
+    ],
+    description='A simple fully working websocket-server in Python',
+    platforms='any',
+)
